60858df80f9f576a2d75e796380c97c18fe2947b
Aleko Konjaria <alekokonjaria71@gmail.com>
2026-04-28 01:46:22 +0400

    Add Github Actions workflow

--- conftest.py
+++ conftest.py
@@ -3,6 +3,9 @@ from selenium import webdriver
 
 @pytest.fixture
 def driver():
+    options = webdriver.FirefoxOptions()
+    options.add_argument("--headless")
+
     driver = webdriver.Firefox()
     driver.maximize_window()
     yield driver
