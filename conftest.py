5fb01e0b9876e00411c9eeb0061ba5092643a7d2
Aleko Konjaria <alekokonjaria71@gmail.com>
2026-04-28 18:57:32 +0400

    Firefox --> Chrome. Edited main.yaml file for chrome

--- conftest.py
+++ conftest.py
@@ -1,11 +1,13 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 
 @pytest.fixture
 def driver():
     options = Options()
     options.add_argument("--headless")
+    options.add_argument("--disable-dev-shm-usage")
 
     driver = webdriver.Chrome()
     driver.maximize_window()
