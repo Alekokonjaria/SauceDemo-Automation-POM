b1b59937e86dcc2e1b4db5cc8c6fc14da25470e4
Aleko Konjaria <alekokonjaria71@gmail.com>
2026-04-28 18:34:23 +0400

    Clean up main.yml and remove headless from terminal

--- conftest.py
+++ conftest.py
@@ -3,9 +3,6 @@ from selenium import webdriver
 
 @pytest.fixture
 def driver():
-    options = webdriver.FirefoxOptions()
-    options.add_argument("--headless")
-
     driver = webdriver.Firefox()
     driver.maximize_window()
     yield driver
