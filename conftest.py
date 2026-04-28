57cea506831920b2c6b77bb6da19c53ed4b99c5b
Aleko Konjaria <alekokonjaria71@gmail.com>
2026-04-28 21:01:44 +0400

    Edited conftest.py and yaml file Vol#2

--- conftest.py
+++ conftest.py
@@ -5,12 +5,12 @@ from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture
 def driver():
-    options = Options()
-    options.add_argument("--headless")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
 
-    driver = webdriver.Chrome(options=options)
-    driver.maximize_window()
+    driver = webdriver.Chrome(options=chrome_options)
     yield driver
     driver.quit()
